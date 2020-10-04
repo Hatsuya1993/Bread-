e100594410a58652adc302cceaa84522fa0f2000
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-10-04 09:46:22 +0800

    starting blog

--- controller/breadController.js
+++ controller/breadController.js
@@ -3,9 +3,43 @@ const catchAsync = require("../utils/catchAsync")
 // Display Home
 
 const homePage = catchAsync(async (req, res) => {
-    res.render("home")
+    res.render("home", {
+        title: "Love with baking items"
+    })
+})
+
+// Display Menu
+
+const menu = catchAsync(async (req, res) => {
+    res.render("menu", {
+        title: "Menu List"
+    })
+})
+
+// Display Team
+
+const team = catchAsync(async (req, res) => {
+    res.render("teams", {
+        title: "Chef Teams"
+    })
+})
+
+// Display Contact
+
+const contact = catchAsync(async (req, res) => {
+    res.render("contact")
+})
+
+// Display Blog
+
+const blog = catchAsync(async (req, res) => {
+    res.render("blog")
 })
 
 module.exports = {
-    homePage
+    homePage,
+    menu,
+    team,
+    contact,
+    blog
 }
--- routes/breadRoute.js
+++ routes/breadRoute.js
@@ -1,9 +1,24 @@
 const express = require("express");
 
-const { homePage } = require("../controller/breadController");
+const {
+    homePage,
+    menu,
+    team,
+    contact,
+    blog
+} = require("../controller/breadController");
 
 const breadRoute = express.Router();
 
 breadRoute.route("/").get(homePage);
+
+breadRoute.route("/menu").get(menu);
+
+breadRoute.route("/team").get(team);
+
+breadRoute.route("/contact").get(contact);
+
+breadRoute.route("/blog").get(blog);
+
 
 module.exports = breadRoute;
